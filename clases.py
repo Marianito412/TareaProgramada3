5fc33b65ac408ed42cb98a39890ccb9b5d1dc297
Marianito412 <Jmarianosoto@gmail.com>
2023-06-04 18:30:28 -0600

    definir clase

--- clases.py
+++ clases.py
@@ -2,3 +2,19 @@
 #Fecha de creacion: 04/06/2023 12:25am
 #Ultima version: 04/06/2023 1:30pm
 #Version: 3.10.6
+
+class Licencia:
+    def  __init__(self):
+        self.cedula = 0
+        self.nombreCompleto = ""
+        self.nacimiento = ""
+        self.expedicion = ""
+        self.vencimiento = ""
+        self.tipoLicencia = ""
+        self.tipoSangre = ""
+        self.esDonador = False
+        self.sede = ""
+        self.puntaje = 0
+        self.correo = ""
+    #@Todo: Getters y setters
+    
