e864a1a64001351dd558664489fed665d4ee8935
Marianito412 <Jmarianosoto@gmail.com>
2023-06-13 17:23:57 -0600

    avances

--- clases.py
+++ clases.py
@@ -17,4 +17,37 @@ class Licencia:
         self.puntaje = 0
         self.correo = ""
     #@Todo: Getters y setters
+
+
+class tipoLicencia:
+    def __init__(self, tipo, subtipo, comentarios, requisitos):
+        self.tipo: str = tipo
+        self.subtipo: str = subtipo
+        self.comentarios: str = comentarios
+        self.requisitos: list = requisitos
+    
+    def __str__(self) -> str:
+        return (f"tipo: {self.tipo}\n"
+                f"Subtipo: {self.subtipo}\n"
+                f"Comentarios: {self.comentarios}\n"
+                f"Requisitos: {self.requisitos}"
+                )
+
+    def getTipo(self):
+        return self.tipo
+    def getSubtipo(self):
+        return self.subtipo
+    def getComentarios(self):
+        return self.comentarios
+    def getRequisitos(self):
+        return self.requisitos
+    
+    def setTipo(self, tipo):
+        self.tipo = tipo
+    def setSubtipo(self, subtipo):
+        self.subtipo = subtipo
+    def setTipo(self, comentarios):
+        self.comentarios = comentarios
+    def setTipo(self, requisitos):
+        self.requisitos = requisitos
     
--- funciones.py
+++ funciones.py
@@ -2,3 +2,40 @@
 #Fecha de creacion: 04/06/2023 12:25am
 #Ultima version: 04/06/2023 1:30pm
 #Version: 3.10.6
+
+import bs4
+import requests
+from clases import tipoLicencia
+
+def crearTag(pEtiqueta, pContenido, pAtributo=""):
+    """
+    Funcionalidad: Crea un string con formato html/xml válido
+    Entradas:
+    -pEtiqueta(str): El nombre de la etiqueta
+    -pContenido(str): El contenido de esa etiqueta
+    -pAtributo(str): Cualquier atributo deseado
+    """
+    pContenido = pContenido.replace("\n", "\n\t")
+    return f"<{pEtiqueta} {pAtributo}>\n\t{pContenido}\n</{pEtiqueta}>"
+
+def conseguirTipoLicencias():
+    url = "https://practicatest.cr/blog/licencias/tipos-licencia-conducir-costa-rica"
+    pagina = requests.get(url).content
+    pagina = bs4.BeautifulSoup(pagina, "html.parser")
+    texto = pagina.find(class_ = "content-blog")
+    Licencias = texto.find_all("h3")[1:]
+    tipos = []
+    for articulo in Licencias:
+        articulo.find
+        tipo = articulo.findPreviousSibling("h2")
+        subtipo = articulo.text.strip()
+        info = articulo.findNextSiblings()[:4]
+        comentario = info[0]
+        requisitos = info[3]
+        tipos.append(tipoLicencia(tipo, subtipo, comentario, requisitos))
+        print(tipos[-1])
+        
+
+
+if __name__ == "__main__":
+    conseguirTipoLicencias()
